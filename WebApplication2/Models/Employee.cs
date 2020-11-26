8d5086f4cbb89979d899a5fd15aa34d95c89966f
red512 <robojinx1@gmail.com>
2020-11-26 19:24:22 +0200

    backend

---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models {
+    public class Employee {
+        public int EmpolyeeId { get; set; }
+        public string Employeename { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
